c4b6c37d915759f475e58b4c3de0bab873039ddc
MarkBracke <mark.bracke@tactics.be>
2021-04-25 14:22:20 +0200

    Removed styling from viewing state

--- src/components/Viewing.svelte
+++ src/components/Viewing.svelte
@@ -2,13 +2,6 @@
     export let url: string;
 </script>
 
-<style>
-    iframe {
-        width: 100vw;
-        height: 100vh;
-    }
-</style>
-
 <div class="viewing">
     <iframe title="miro board" src={url}></iframe>
 </div>
